49815cf4f3b38685fed47c987a53cb09e47be471
abrakitlaw <gkitlaw@gmail.com>
2018-07-08 01:39:20 +0700

    new class added

---
+++ app/src/main/java/com/thesis/application/echo/view/SaveUserInformation.java
@@ -0,0 +1,15 @@
+package com.thesis.application.echo.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.thesis.application.echo.R;
+
+public class SaveUserInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_user_information);
+    }
+}
